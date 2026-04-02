37c5b2a6ff7b04f41c5eade2870b6fa2ff13dcee
Willem van Haaps <wvan0006@student.monash.edu>
2026-04-02 11:32:35 +1100

    Conflicts resolved

--- calc.py
+++ calc.py
@@ -7,13 +7,10 @@ def compute(expression):
         return num0 + num1
     elif operator == '-':
         return num0 - num1
-<<<<<<< HEAD
     elif operator == '*':
         return num0 * num1
-=======
     elif operator == '/':
         return num0 / num1
->>>>>>> add-division
     else:
         print('unknown operator!')
         return None
